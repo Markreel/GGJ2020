8ebb3d6a01e324dd6a07148ca1bfc0f6fc3237bd
Ruben Hooijer <loljoch@gmail.com>
2020-02-01 15:39:55 +0100

    Working on targetMovement

--- GGJ2020/Assets/Scripts/AimTarget.cs
+++ GGJ2020/Assets/Scripts/AimTarget.cs
@@ -18,10 +18,10 @@ public class AimTarget : MonoBehaviour
 
     private void Update()
     {
-        DrawLine();
+        DrawQuadLine();
     }
 
-    private void DrawLine()
+    private void DrawQuadLine()
     {
         Vector3 p0 = transform.position / 2;
         Vector3 p2 = slime.position / 2;
@@ -30,6 +30,34 @@ public class AimTarget : MonoBehaviour
         p1.y += lineHeight;
         p1.x += midPointPosition;
         p1.z += midPointPosition;
+
+        ExtDebug.DrawBox(p1, new Vector3(0.5f, 0.5f, 0.5f), Quaternion.identity, Color.blue);
+
+        List<Vector3> _points = new List<Vector3>();
+        for (float i = 0; i < 1; i += 0.02f)
+        {
+            _points.Add(CalcQuadraticBezierCurve(i, p0, p1, p2));
+        }
+        Points = _points.ToArray();
+        lr.SetPositions(Points);
+    }
+
+    private void DrawCubicLine()
+    {
+        Vector3 p0 = transform.position / 2;
+        Vector3 p3 = slime.position / 2;
+
+        Vector3 midPoint = (p0) + (p3);
+
+        Vector3 p2 = (p0 /2) + (midPoint/2);
+        Vector3 p1 = (p0) + (p2);
+
+        p1.y += lineHeight;
+        p1.x += midPointPosition;
+        p1.z += midPointPosition;
+
+        ExtDebug.DrawBox(p1, new Vector3(0.5f, 0.5f, 0.5f), Quaternion.identity, Color.blue);
+
         List<Vector3> _points = new List<Vector3>();
         for (float i = 0; i < 1; i += 0.02f)
         {
@@ -41,7 +69,14 @@ public class AimTarget : MonoBehaviour
 
     private Vector3 CalcQuadraticBezierCurve(float t, Vector3 p0, Vector3 p1, Vector3 p2)
     {
-        Vector3 point = (1 - t) * 2 * p0 + 2 * (1 - t) * t * p1 + t * 2 * p2;
+        Vector3 point = (1 - t) * 2 * p0 + 2 * (1 - t) * t * p1 + t * p2;
+        return point;
+    }
+
+    private Vector3 CalcCubicBezierCurve(float t, Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3)
+    {
+        Vector3 point = (1 - t) * 3 * p0 + 3 * (1 - t) * 2 * 1 * p1 + 3 * (1 - t) * t * 2 * p2 + t * 3 * p3;
+
         return point;
     }
 }
--- GGJ2020/Assets/Scripts/TargetMovement.cs
+++ GGJ2020/Assets/Scripts/TargetMovement.cs
@@ -6,6 +6,7 @@ public class TargetMovement : MonoBehaviour
 {
     [Header("Stats")]
     public float speed;
+    public float angleThreshold = 15f;
     public float rayHeight = 10f;
     public float rayLength = 2f;
     public float yThreshold = 0.2f;
@@ -19,6 +20,13 @@ public class TargetMovement : MonoBehaviour
     private Vector3 pos;
     private float vInput = 0f;
     private float hInput = 0f;
+
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     private void Start()
     {
@@ -34,10 +42,8 @@ public class TargetMovement : MonoBehaviour
     private void Update()
     {
         GetInput();
-        if (vInput + hInput != 0)
-        {
-            Movement();
-        }
+        Movement();
+        
     }
 
     private void GetInput()
@@ -48,34 +54,114 @@ public class TargetMovement : MonoBehaviour
 
     private void Movement()
     {
-        Vector3 wantedPos = pos;
-
         Quaternion lookDir = Quaternion.Euler(transform.rotation.eulerAngles.x, forwardTranform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
         transform.rotation = lookDir;
 
-        Vector3 moveDir = transform.forward * vInput;
-        moveDir += transform.right * hInput;
-        moveDir.y = 0;
-        moveDir = moveDir.normalized;
+        Vector3 moveVelocity = Vector3.zero;
+        moveVelocity += transform.forward * vInput * speed;
+        moveVelocity += transform.right * hInput * speed;
 
-        wantedPos += moveDir * speed;
+        moveVelocity = moveVelocity.normalized * speed;
 
-        if (Physics.Raycast(wantedPos + Vector3.up * rayHeight, Vector3.down * rayLength, out RaycastHit hit))
-        {
-            Vector3 newPos = hit.point;
-            if (newPos.y < transform.position.y - yThreshold || newPos.y > transform.position.y + yThreshold)
-            {
-                newPos.x = transform.position.x;
-                newPos.z = transform.position.z;
-                ExtDebug.DrawBox(newPos, transform.localScale / 2, Quaternion.identity, Color.blue);
-                newPos.y = (newPos.y > transform.position.y)? transform.position.y + speed : transform.position.y - speed;
-            }
-            SetPos(newPos);
-        }
+        rb.velocity = moveVelocity;
+
+        Quaternion WantedRotation = Quaternion.LookRotation(moveVelocity);
+        transform.localRotation = WantedRotation;
+
+        Debug.DrawRay(transform.position, transform.forward, Color.red, 2f);
+
+        //Vector3 wantedPos = pos;
+
+        //Vector3 moveDir = transform.forward * vInput;
+        //moveDir += transform.right * hInput;
+        //moveDir.y = 0;
+        //moveDir = moveDir.normalized;
+
+        //wantedPos += moveDir * speed;
+
+        //if (Physics.Raycast(wantedPos + Vector3.up * rayHeight, Vector3.down * rayLength, out RaycastHit hit))
+        //{
+        //    Vector3 newPos = hit.point;
+        //    if (newPos.y < transform.position.y - yThreshold || newPos.y > transform.position.y + yThreshold)
+        //    {
+        //        newPos.x = transform.position.x;
+        //        newPos.z = transform.position.z;
+        //        newPos.y = (newPos.y > transform.position.y)? transform.position.y + speed : transform.position.y - speed;
+        //    }
+        //    SetPos(newPos);
+        //}
     }
 
     private void OnEnterAimMode()
     {
         SetPos(forwardTranform.forward);
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+
+        List<ContactPoint> contactPoints = new List<ContactPoint>();
+        collision.GetContacts(contactPoints);
+        contactPoints.RemoveAt(collision.contactCount);
+        if (contactPoints.Count > 0)
+        {
+            CheckOnCollisionHits(contactPoints);
+        }
+    }
+
+    private void CheckOnCollisionHits(List<ContactPoint> contactPoints)
+    {
+        for (int i = 0; i < contactPoints.Count; i++)
+        {
+            if (contactPoints[i].otherCollider == null) break;
+
+            Vector3 dir = contactPoints[i].normal;
+
+
+
+            //Ceiling
+            //else if (Vector3.Angle(dir, Vector3.down) <= groundCollAngleThreshold && Vector3.Angle(dir, Vector3.down) >= -groundCollAngleThreshold)
+            //{
+            //    break;
+            //}
+        }
+
+
+        for (int i = 0; i < contactPoints.Count; i++)
+        {
+            if (contactPoints[i].otherCollider == null) break;
+
+            Vector3 dir = contactPoints[i].normal;
+
+            //Ground
+            if (Vector3.Angle(dir, Vector3.up) <= angleThreshold && Vector3.Angle(dir, Vector3.up) >= -angleThreshold)
+            {
+                return;
+            }
+
+            //Right
+            if (Vector3.Angle(dir, -transform.right) <= angleThreshold && Vector3.Angle(dir, -transform.right) >= -angleThreshold)
+            {
+
+                break;
+            }
+
+            //Left
+            else if (Vector3.Angle(dir, transform.right) <= angleThreshold && Vector3.Angle(dir, transform.right) >= -angleThreshold)
+            {
+
+                break;
+            }
+
+            //Forward
+            else if (Vector3.Angle(dir, -transform.forward) <= angleThreshold && Vector3.Angle(dir, -transform.forward) >= -angleThreshold)
+            {
+                Debug.Log("Something in front");
+                break;
+            }
+        }
+        
+
+
     }
 }
