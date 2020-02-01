fee6d743b8563bf9daf3dce87c68afdc41ad4965
Ruben Hooijer <loljoch@gmail.com>
2020-02-01 19:56:37 +0100

    Added BlobTrail

---
+++ GGJ2020/Assets/Scripts/UI/SceneManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneRegulator : MonoBehaviour
+{
+    private SceneRegulator instance;
+    private SceneRegulator Instance { get { return instance; } set { instance = value; } }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        } else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public enum Scenes
+    {
+        StartLevel,
+        TweedeLevel,
+        DerdeLevel
+
+    }
+
+    public static void SwitchScene(Scenes scene)
+    {
+        SceneManager.LoadScene((int)scene);
+    }
+}
