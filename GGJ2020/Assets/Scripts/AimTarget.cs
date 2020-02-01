50b80af906eed9448108a70e3935446722509d03
Ruben Hooijer <loljoch@gmail.com>
2020-02-01 10:34:39 +0100

    Movement, BEZIER LINE

---
+++ GGJ2020/Assets/Scripts/AimTarget.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimTarget : MonoBehaviour
+{
+    public Transform slime;
+    public float lineHeight = 2;
+    public float midPointPosition = 0;
+    private LineRenderer lr;
+
+    private void Awake()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+
+    private void Update()
+    {
+        DrawLine();
+    }
+
+    private void DrawLine()
+    {
+        Vector3 p0 = transform.position / 2;
+        Vector3 p2 = slime.position / 2;
+        Vector3 p1 = (p0 / 2) + (p2 / 2);
+        p1.y += lineHeight;
+        p1.x += midPointPosition;
+        p1.z += midPointPosition;
+        List<Vector3> points = new List<Vector3>();
+        for (float i = 0; i < 1; i += 0.02f)
+        {
+            points.Add(CalcQuadraticBezierCurve(i, transform.position/2, p1, slime.position/2));
+        }
+
+        lr.SetPositions(points.ToArray());
+    }
+
+    private Vector3 CalcQuadraticBezierCurve(float t, Vector3 p0, Vector3 p1, Vector3 p2)
+    {
+        Vector3 point = (1 - t) * 2 * p0 + 2 * (1 - t) * t * p1 + t * 2 * p2;
+        return point;
+    }
+}
---
+++ GGJ2020/Assets/Scripts/Button.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Button : MonoBehaviour
+{
+    public bool buttonState = false;
+    public UnityEvent OnTrigger;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<SlimeMovement>())
+        {
+            OnTrigger?.Invoke();
+        }
+    }
+}
