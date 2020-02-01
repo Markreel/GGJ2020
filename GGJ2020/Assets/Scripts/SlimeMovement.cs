49f2b4dc2dc07de9fd76c98edb37bb64e85b5f7d
Ruben Hooijer <loljoch@gmail.com>
2020-02-01 11:01:31 +0100

    Added TargetMovement script

--- GGJ2020/Assets/Scripts/SlimeMovement.cs
+++ GGJ2020/Assets/Scripts/SlimeMovement.cs
@@ -5,48 +5,6 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class SlimeMovement : Movement
 {
-    //[Header("Stats")]
-    //public float movementSpeed;
-    //public float gravityAmplifier;
-
-    //[Header("Input")]
-    //public string horizontalJoystick = "Horizontal";
-    //public string verticalJoystick = "Vertical";
-    //public Transform forwardTranform;
-
-    //private float vInput = 0f;
-    //private float hInput = 0f;
-
-    //private Rigidbody rb;
-
-    //private void Awake()
-    //{
-    //    rb = GetComponent<Rigidbody>();
-    //}
-
-    //private void FixedUpdate()
-    //{
-    //    Move();
-
-    //    CustomGravity();
-    //}
-
-    //private void Update()
-    //{
-    //    GetInput();
-    //}
-
-    //private void GetInput()
-    //{
-    //    vInput = Input.GetAxis(verticalJoystick);
-    //    hInput = Input.GetAxis(horizontalJoystick);
-    //}
-
-    //private void CustomGravity()
-    //{
-    //    rb.velocity = new Vector3(rb.velocity.x, -gravityAmplifier, rb.velocity.z);
-    //}
-
     protected override Vector3 Move()
     {
         Vector3 vel = base.Move();
--- GGJ2020/Assets/Scripts/TargetMovement.cs
+++ GGJ2020/Assets/Scripts/TargetMovement.cs
@@ -2,57 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
-public class TargetMovement : MonoBehaviour
+public class TargetMovement : Movement
 {
-    [Header("Stats")]
-    public float movementSpeed;
-
-    [Header("Input")]
-    public string horizontalJoystick = "Horizontal";
-    public string verticalJoystick = "Vertical";
-    public Transform forwardTranform;
-
-    private float vInput = 0f;
-    private float hInput = 0f;
-
-    private Rigidbody rb;
-
-    private void Awake()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
-
-    private void FixedUpdate()
-    {
-        Move();
-    }
-
-    void Update()
-    {
-        GetInput();
-    }
-
-    private void GetInput()
-    {
-        vInput = Input.GetAxis(verticalJoystick);
-        hInput = Input.GetAxis(horizontalJoystick);
-    }
-
-    private void Move()
-    {
-        Vector3 moveVelocity = Vector3.zero;
-
-        Quaternion walkDir = Quaternion.Euler(transform.rotation.eulerAngles.x, forwardTranform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
-        transform.rotation = walkDir;
-
-        moveVelocity += transform.forward * vInput;
-        moveVelocity += transform.right * hInput;
-
-
-        moveVelocity = moveVelocity.normalized;
-        moveVelocity *= movementSpeed;
-
-        rb.velocity = moveVelocity;
-    }
+    
 }
