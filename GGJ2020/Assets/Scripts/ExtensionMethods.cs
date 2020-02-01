7291510a102955913d410a688bb94b8cf7bff951
Ruben Hooijer <loljoch@gmail.com>
2020-02-01 14:30:44 +0100

    Added walk over wall

---
+++ GGJ2020/Assets/Scripts/ExtensionMethods.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public static class ExtensionMethods
+{
+    public static bool BoxCast(this Physics physics, BoxCast boxCast)
+    {
+        return Physics.BoxCast(boxCast.center, boxCast.halfExtents, boxCast.direction);
+    }
+
+    public static bool BoxCast(this Physics physics, BoxCast boxCast, out RaycastHit hitInfo)
+    {
+        return Physics.BoxCast(boxCast.center, boxCast.halfExtents, boxCast.direction, out hitInfo);
+    }
+
+    public static bool CheckBox(this Physics physics, BoxCast boxCast)
+    {
+        return Physics.CheckBox(boxCast.center, boxCast.halfExtents);
+    }
+}
