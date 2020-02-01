716d7efe813df56654a68e5a259e58eb8b4991cb
Markreel <markmeerssman@live.nl>
2020-02-01 19:13:25 +0100

    Started working on merge mechanic

--- GGJ2020/Assets/Scripts/Slime.cs
+++ GGJ2020/Assets/Scripts/Slime.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Slime : MonoBehaviour
 {
+    [SerializeField] LayerMask slimeMask;
+    [SerializeField] float maxSlimeDetectionRange = 3;
+
     [SerializeField] float scaleFactor;
     public bool PrimeSlime = false;
 
@@ -12,6 +15,11 @@ public class Slime : MonoBehaviour
     private void Awake()
     {
         UpdateSlimeScale();
+    }
+
+    private void Update()
+    {
+        CheckForNearbySlimes();
     }
 
     public void AddBlobToSlime()
@@ -26,8 +34,32 @@ public class Slime : MonoBehaviour
         transform.localScale = Vector3.one * SlimeSize * scaleFactor;
     }
 
-    public void MergeWithOtherSlime()
+    public void MergeWithOtherSlime(Slime _otherSlime)
     {
+        SlimeSize += _otherSlime.SlimeSize;
+ 
+    }
 
+    private void CheckForNearbySlimes()
+    {
+        if (!PrimeSlime)
+        {
+            RaycastHit[] _sphereHits = Physics.SphereCastAll(transform.position, maxSlimeDetectionRange, transform.forward, slimeMask.value);
+
+            foreach (var _hit in _sphereHits)
+            {
+                _hit.transform.Translate(transform.position);
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Slime _slime = collision.transform.GetComponent<Slime>();
+
+        if (_slime != null)
+        {
+            MergeWithOtherSlime(_slime);
+        }
     }
 }
