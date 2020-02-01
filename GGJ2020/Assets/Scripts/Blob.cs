5d47235c983768d9df17eed8caa879ed4f0f3d96
Markreel <markmeerssman@live.nl>
2020-02-01 10:37:11 +0100

    Added Cinemachine Added Camera Behaviour

---
+++ GGJ2020/Assets/Scripts/Blob.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blob : MonoBehaviour
+{
+    void Update()
+    {
+        transform.position += Vector3.forward;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
---
+++ GGJ2020/Assets/Scripts/SlimeTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class SlimeTest : MonoBehaviour
+{
+    [SerializeField] List<GameObject> slimeList = new List<GameObject>();
+
+
+    //[SerializeField] CinemachineVirtualCamera vCam;
+    [SerializeField] GameObject newSlimePrefab;
+
+    [Header("Input")]
+    [SerializeField] string horizontalInputString = "Horizontal";
+    [SerializeField] string verticalInputString = "Vertical";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void ShootBlob()
+    {
+        
+    }
+
+
+
+}
