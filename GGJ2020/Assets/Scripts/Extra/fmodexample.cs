40094da79ae35e6ec7bed79fbd9c914f48106fd8
Markreel <markmeerssman@live.nl>
2020-02-01 21:32:37 +0100

    Final FMOD build

---
+++ GGJ2020/Assets/Scripts/Extra/fmodexample.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fmodexample : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public string LanaGra;
+    FMOD.Studio.EventInstance audioClip;
+
+    void Awake()
+    {
+        audioClip = FMODUnity.RuntimeManager.CreateInstance(LanaGra);
+        audioClip.start();
+
+        StartCoroutine(IETest());
+    }
+
+    void Update()
+    {
+
+
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(audioClip, GetComponent<Transform>(), GetComponent<Rigidbody>());
+    }
+
+    private IEnumerator IETest()
+    {
+        yield return new WaitForSeconds(3);
+
+        audioClip.start();
+
+        StartCoroutine(IETest());
+
+        yield return null;
+    }
+}
+
