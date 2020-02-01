aea9e9090f199b3ce297e82a4b4bc39f23c3b400
Ruben Hooijer <loljoch@gmail.com>
2020-02-01 21:40:07 +0100

    Commit slime upgrade etc

---
+++ GGJ2020/Assets/Scripts/UI/OnFadeEnd.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnFadeEnd : MonoBehaviour
+{
+    public UnityEvent OnAnimationEnd;
+
+    public void AnimationEnd()
+    {
+        OnAnimationEnd?.Invoke();
+    }
+}
